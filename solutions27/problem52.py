ce7435d7a1e229206beb6989bccd44df5b15a22a
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2023-04-22 15:19:51 +0300

    Задача 27.52

--- solutions27/problem52.py
+++ solutions27/problem52.py
@@ -1,14 +1,16 @@
 from random import randint
 
+
 def bruteforce(a):
     answer = 0
     for i1 in range(len(a)):
         for i2 in range(i1 + 1, len(a)):
             for i3 in range(i2 + 1, len(a)):
-                s = a[i1] + a[i2] + a[i3]  
+                s = a[i1] + a[i2] + a[i3]
                 if s > answer and s % 3 == 0:
                     answer = s
     return answer
+
 
 def efficient(a):
     m = [
@@ -18,9 +20,17 @@ def efficient(a):
     ]
 
     for x in a:
-        if x < m[0]:
-            m[x % 3]
-    
+        m[x % 3].append(x)
+        m[x % 3].sort(reverse = True)
+        m[x % 3] = m[x % 3][:3]
+
+    return max([
+        m[0][0] + m[1][0] + m[2][0], # выбираем максимальное число по каждому остатку 0, 1, 2.  Тогда сумма имеет остаток (0 + 1 + 2) % 3 = 0.
+        sum(m[0]), # Выбираем три максимальных числа, делящихся на 3. Тогда сумма тоже делится на 3.
+        sum(m[1]), # Выбираем три максимальных числа, дающих остаток 1 при делении на 3. Тогда сумма имеет остаток (1 + 1 + 1) % 3 = 0.
+        sum(m[2]), # Выбираем три максимальных числа, дающих остаток 2 при делении на 3. Тогда сумма имеет остаток (2 + 2 + 2) % 3 = 0.
+    ])
+
 def test_bruteforce():
     test_cases = [
         ([1, 3, 6, 9], 9 + 6 + 3),
@@ -32,6 +42,7 @@ def test_bruteforce():
         assert actual == answer, f'Не пройден тест {data}: ожидалось {answer}, получено {actual}'
     print('Bruteforce тесты пройдены')
 
+
 def test_efficient(a):
     """
     Тестирует решения на заданном массиве.
@@ -39,17 +50,20 @@ def test_efficient(a):
     expected = bruteforce(a)
     actual = efficient(a)
     assert expected == actual, f'Expected {expected}, actual {actual}'
+    return actual
 
-def random_tests(test_count = 20):
+
+def random_tests(test_count=20):
     print(f'Запуск {test_count} случайных тестов...')
     for test_id in range(1, test_count+1):
         # генерирурем случайные данные
         n = 12
         a = [randint(1, 100) for i in range(n)]
-        test_efficient(a)
-        print(f"Пройден тест #{test_id} на данных {a}")
-    
+        answer = test_efficient(a)
+        print(f"Пройден тест #{test_id} на данных {a}. Ответ {answer}")
+
     print(f"Случайные тесты пройдены!")
+
 
 dataA = []
 dataB = []
@@ -68,7 +82,7 @@ print(f'Длина массива B:', len(dataB))
 test_bruteforce()
 print(f'bruteforce, список A:', bruteforce(dataA))
 
-random_tests()
+random_tests(test_count=1000)
 
 print(f'efficient, список A:', efficient(dataA))
 print(f'efficient, список B:', efficient(dataB))
